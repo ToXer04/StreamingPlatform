7e4612fe50cea6a8845161b8bd619ae30dc34a27
ToXer04 <simonefarina2004@gmail.com>
2024-03-13 19:08:46 +0100

    Create UserService.java

---
+++ Streaming-website/src/main/java/tryxe/co/Streaming/website/services/UserService.java
@@ -0,0 +1,17 @@
+package tryxe.co.Streaming.website.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tryxe.co.Streaming.website.entities.User;
+import tryxe.co.Streaming.website.repositories.UserRepository;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepository userRepository;
+}
