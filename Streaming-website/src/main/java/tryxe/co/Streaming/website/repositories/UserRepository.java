d300bfcec56826ff7404dada76fcdddb40cdca6d
ToXer04 <simonefarina2004@gmail.com>
2024-03-13 19:08:22 +0100

    created user entity

---
+++ Streaming-website/src/main/java/tryxe/co/Streaming/website/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package tryxe.co.Streaming.website.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import tryxe.co.Streaming.website.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
