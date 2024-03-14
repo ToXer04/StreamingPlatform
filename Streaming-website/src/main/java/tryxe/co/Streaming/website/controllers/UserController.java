30a00aa2a9f0749018d2415f942cbd85239cf5e7
ToXer04 <simonefarina2004@gmail.com>
2024-03-14 03:08:13 +0100

    delete method in UserService

--- Streaming-website/src/main/java/tryxe/co/Streaming/website/controllers/UserController.java
+++ Streaming-website/src/main/java/tryxe/co/Streaming/website/controllers/UserController.java
@@ -21,16 +21,19 @@ public class UserController {
     ) {
         return userService.create(user);
     }
+
     @GetMapping("/get/{id}")
     public Optional<User> get(
             @PathVariable Long id
     ) {
         return userService.get(id);
     }
+
     @GetMapping("/get-all")
     public List<User> getAll() {
         return userService.getAll();
     }
+
     @PutMapping("/update/{id}")
     public User update(
             @PathVariable Long id,
@@ -38,6 +41,7 @@ public class UserController {
     ) {
         return userService.update(id, user);
     }
+
     @PatchMapping("/update/{id}/name")
     public User updateName(
             @PathVariable Long id,
@@ -45,6 +49,7 @@ public class UserController {
     ) throws Exception {
         return userService.updateName(id, value);
     }
+
     @PatchMapping("/update/{id}/surname")
     public User updateSurname(
             @PathVariable Long id,
@@ -52,6 +57,7 @@ public class UserController {
     ) throws Exception {
         return userService.updateSurname(id, value);
     }
+
     @PatchMapping("/update/{id}/email")
     public User updateEmail(
             @PathVariable Long id,
@@ -59,6 +65,7 @@ public class UserController {
     ) throws Exception {
         return userService.updateEmail(id, value);
     }
+
     @PatchMapping("/update/{id}/birth-date")
     public User updateBirthDate(
             @PathVariable Long id,
@@ -66,6 +73,7 @@ public class UserController {
     ) throws Exception {
         return userService.updateBirthDate(id, value);
     }
+
     @PatchMapping("/update/{id}/country")
     public User updateCountry(
             @PathVariable Long id,
@@ -73,6 +81,7 @@ public class UserController {
     ) throws Exception {
         return userService.updateCountry(id, value);
     }
+
     @PatchMapping("/update/{id}/prefix")
     public User updatePrefix(
             @PathVariable Long id,
@@ -80,6 +89,7 @@ public class UserController {
     ) throws Exception {
         return userService.updatePrefix(id, value);
     }
+
     @PatchMapping("/update/{id}/phone-number")
     public User updatePhoneNumber(
             @PathVariable Long id,
@@ -87,7 +97,8 @@ public class UserController {
     ) throws Exception {
         return userService.updatePhoneNumber(id, value);
     }
-//TODO
+
+    //TODO
 //    @PatchMapping("/update/{id}/membership")
 //    public User updateMembership(
 //            @PathVariable Long id,
--- Streaming-website/src/main/java/tryxe/co/Streaming/website/services/UserService.java
+++ Streaming-website/src/main/java/tryxe/co/Streaming/website/services/UserService.java
@@ -15,35 +15,42 @@ import java.util.Optional;
 public class UserService {
     @Autowired
     UserRepository userRepository;
+
     public User create(User user) {
         user.setRegistrationDate(LocalDate.now());
         return userRepository.saveAndFlush(user);
     }
+
     public Optional<User> get(Long id) {
         return userRepository.findById(id);
     }
+
     public List<User> getAll() {
         return userRepository.findAll();
     }
+
     public User update(Long id, User user) {
         user.setId(id);
         return userRepository.save(user);
     }
+
     public User updateName(Long id, String name) throws Exception {
         Optional<User> user = userRepository.findById(id);
-        if(user.isEmpty()) throw new Exception("User with id " + id + " doesn't exist");
+        if (user.isEmpty()) throw new Exception("User with id " + id + " doesn't exist");
         user.get().setName(name);
         return userRepository.save(user.get());
     }
+
     public User updateSurname(Long id, String surname) throws Exception {
         Optional<User> user = userRepository.findById(id);
-        if(user.isEmpty()) throw new Exception("User with id " + id + " doesn't exist");
+        if (user.isEmpty()) throw new Exception("User with id " + id + " doesn't exist");
         user.get().setSurname(surname);
         return userRepository.save(user.get());
     }
+
     public User updateEmail(Long id, String email) throws Exception {
         Optional<User> user = userRepository.findById(id);
-        if(user.isEmpty()) throw new Exception("User with id " + id + " doesn't exist");
+        if (user.isEmpty()) throw new Exception("User with id " + id + " doesn't exist");
         try {
             user.get().setEmail(email);
             return userRepository.save(user.get());
@@ -51,37 +58,41 @@ public class UserService {
             throw new Exception("The email " + email + " is already associated to an account");
         }
     }
+
     public User updateBirthDate(Long id, String birthDate) throws Exception {
         Optional<User> user = userRepository.findById(id);
-        if(user.isEmpty()) throw new Exception("User with id " + id + " doesn't exist");
-        try{
+        if (user.isEmpty()) throw new Exception("User with id " + id + " doesn't exist");
+        try {
             user.get().setBirthDate(LocalDate.parse(birthDate));
             return userRepository.save(user.get());
         } catch (Exception exception) {
             throw new Exception("Wrong format, it must be yyyy-mm-dd");
         }
     }
+
     public User updateCountry(Long id, String country) throws Exception {
         Optional<User> user = userRepository.findById(id);
-        if(user.isEmpty()) throw new Exception("User with id " + id + " doesn't exist");
+        if (user.isEmpty()) throw new Exception("User with id " + id + " doesn't exist");
         user.get().setCountry(country);
         return userRepository.save(user.get());
     }
+
     public User updatePrefix(Long id, String prefix) throws Exception {
         Optional<User> user = userRepository.findById(id);
-        if(user.isEmpty()) throw new Exception("User with id " + id + " doesn't exist");
-        try{
+        if (user.isEmpty()) throw new Exception("User with id " + id + " doesn't exist");
+        try {
             user.get().setPrefix(Long.parseLong(prefix));
             return userRepository.save(user.get());
         } catch (Exception exception) {
             throw new Exception("Wrong format, it must be a numeric value");
         }
     }
+
     public User updatePhoneNumber(Long id, String phoneNumber) throws Exception {
         Optional<User> user = userRepository.findById(id);
-        if(user.isEmpty()) throw new Exception("User with id " + id + " doesn't exist");
+        if (user.isEmpty()) throw new Exception("User with id " + id + " doesn't exist");
         try {
-            if(phoneNumber.length() < 7 || phoneNumber.length() > 15) throw new NumberFormatException();
+            if (phoneNumber.length() < 7 || phoneNumber.length() > 15) throw new NumberFormatException();
             user.get().setPhoneNumber(BigInteger.valueOf(Long.parseLong((phoneNumber))));
             return userRepository.save(user.get());
         } catch (NumberFormatException nfe) {
@@ -90,10 +101,16 @@ public class UserService {
             throw new Exception("The phone number " + phoneNumber + " is already associated to an account");
         }
     }
-//TODO
+
+    //TODO
 //    public User updateMembership(Long id, String membershipId) throws Exception {
 //        Optional<User> user = userRepository.findById(id);
 //        if(user.isEmpty()) throw new Exception("User with id " + id + " doesn't exist");
 //        return null;
 //    }
+    public void delete(Long id) throws Exception {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isEmpty()) throw new Exception("User with id " + id + " doesn't exist");
+        userRepository.delete(user.get());
+    }
 }
